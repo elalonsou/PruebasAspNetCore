6be50e55c2a081812c1fdd3e2c552b42ccc88e99
Sergio <elalonsou@hotmail.com>
2019-05-31 13:16:35 +0200

    primer comit

---
+++ PruebaColeccionGenerica/DiccionarioRecorribleGenerico.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PoCColeccionGenerica
+{
+    class DiccionarioRecorribleGenerico<TKey, TValue> 
+    {
+        private Dictionary<TKey, TValue> _diccionario;
+        private List<TKey> _listaClaves;
+        public DiccionarioRecorribleGenerico()
+        {
+            _diccionario = new Dictionary<TKey, TValue>();
+            _listaClaves = new List<TKey>();
+        }
+           
+        private int _contPos = 0;
+
+
+        public void Añadir(TKey clave, TValue valor)
+        {
+            _diccionario.Add(clave, valor);
+            _listaClaves.Add(clave);
+        }
+
+        public void MoverPrimero()
+        {
+            _contPos=0;
+        }
+
+        public int Cantidad()
+        {
+            return _diccionario.Count;
+        }
+
+        public bool EsUltimo()
+        {
+            if (_contPos + 1 >= _diccionario.Count || _diccionario.Count ==0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void MoverSiguiente()
+        {
+            _contPos += 1;
+        }
+
+        public void MoverAnterior()
+        {
+            _contPos -= 1;
+        }
+
+        public TValue ObtenerActual()
+        {
+            return _diccionario[_listaClaves[_contPos]];
+        }
+
+        public TValue ObtenerValorPorClave(TKey clave)
+        {
+            return _diccionario[clave];
+        }
+
+        //Indexer
+        public TValue this[TKey clave]
+        {
+            get
+            {
+                return _diccionario [clave];
+            }
+        }
+
+        //------ Prueba uso de Yield ------
+        // 
+        public IEnumerable<TValue> Valores
+        {
+            get {
+                foreach (TValue valor in _diccionario.Values)
+                {
+                    yield return valor;
+                }
+            }    
+        }
+    }
+}
---
+++ PruebaColeccionGenerica/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace PoCColeccionGenerica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DiccionarioRecorribleGenerico<int, string> dicc = new DiccionarioRecorribleGenerico<int, string>();
+            dicc.Añadir(1, "Hola 1");
+            dicc.Añadir(2, "Hola 2");
+            dicc.Añadir(3, "Hola 3");
+
+            bool fin = false;
+
+            if (dicc.Cantidad() != 0)
+            {
+                dicc.MoverPrimero();
+                while (!fin)
+                {
+                    Console.WriteLine($"Elemento Actual: {dicc.ObtenerActual()}");
+
+                    if (dicc.EsUltimo())
+                    {
+                        fin = true;
+                    }
+                    else
+                    {
+                        dicc.MoverSiguiente();
+                    }
+                }
+                Console.WriteLine($"Obteniendo elemento por clave 2: {dicc.ObtenerValorPorClave(2)}");
+                Console.WriteLine($"Obteniendo elemento mediante Indexer 3: {dicc[3]}");
+
+                Console.WriteLine("-------- Probando Iteracion con Yield  -----------\n");
+                foreach (string item in dicc.Valores)
+                {
+                    Console.WriteLine($"Valor obtenido con yield: {item}");
+                }
+
+
+                //Console.WriteLine($"Obteniendo elemento por clave no existente: {dicc.ObtenerValorPorClave(6)}");
+                Console.WriteLine("Terminado ");
+            }
+        }
+    }
+}
