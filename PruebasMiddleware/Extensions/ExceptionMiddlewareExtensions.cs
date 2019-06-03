74445970087152a25c56be4e27bccd6b15b281f3
elalonsou <elalonsou@hotmail.com>
2019-06-03 15:27:01 +0200

    Agregado proyecto pruebasMiddleware

---
+++ PruebasMiddleware/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,39 @@
+using PruebasMiddleware.Models;
+using LoggerService;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+
+namespace PruebasMiddleware.Extensions
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        //Al poner this delante de IApplicationBuilder se esta configurando como una extension
+        //del tipo IApplicationBuilder de forma que las instancias de este tipo podran usar
+        //el metodo de la forma     app.ConfigureExceptionHandler()
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app, ILoggerManager logger)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+ 
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if(contextFeature != null)
+                    { 
+                        logger.LogError($"Something went wrong: {contextFeature.Error}");
+ 
+                        await context.Response.WriteAsync(new ErrorDetails()
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = "Internal Server Error."
+                        }.ToString());
+                    }
+                });
+            });
+        } 
+    }
+}
