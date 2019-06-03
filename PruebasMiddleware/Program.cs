ae851b59fb47177e6eea932766c44022b25cc385
elalonsou <elalonsou@hotmail.com>
2019-06-03 19:09:28 +0200

    Agregando proveedor logging a mitad

--- PruebasMiddleware/Program.cs
+++ PruebasMiddleware/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Debug;
 
 namespace PruebasMiddleware
 {
@@ -21,6 +22,17 @@ namespace PruebasMiddleware
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .ConfigureLogging((hostingContext,logging) =>
+                    {
+                        logging.AddConsole();
+                        logging.AddDebug();
+                        //Ejemplo de filto
+                        //El segundo AddFilter especifica el proveedor de depuración mediante su nombre de tipo. 
+                        //El primer AddFilter se aplica a todos los proveedores, dado que no especifica un tipo de proveedor.
+                       logging.AddFilter("System", LogLevel.Debug);
+                       logging.AddFilter<DebugLoggerProvider>("Microsoft", LogLevel.Trace);
+                    }
+                );
     }
 }
