41f01803cef0ae1a759194a16540b16b8aa91ac0
Sergio <elalonsou@hotmail.com>
2020-04-24 20:34:17 +0200

    Agregado metodo put

--- PruebasAPI_CRUD_EntityFramework/Controllers/CancionesController.cs
+++ PruebasAPI_CRUD_EntityFramework/Controllers/CancionesController.cs
@@ -65,11 +65,43 @@ namespace PruebasAPI_CRUD_EntityFramework.Controllers
             return Ok(cancion);
         }
 
-        [HttpPut]
-        public async Task<ActionResult<Cancion>> PutCancion()
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Cancion>> PutCancion(int id,[FromBody] Cancion cancion)
         {
-            //TODO Put Cancion
-            throw new NotImplementedException();
+            //Comprobamos que el id pasado en la entidad y el id por parametro es el mismo.
+            //Si usamos DTO, el Id lo quitariamos del DTO porque no es un dato que se informe en el DTO
+            //y al mapearlo con la entidad se lo pondriamos desde el parametro
+            if (id != cancion.Id)
+            {
+                return BadRequest();
+            }
+
+            //Ahora agregamos un cambio al contexto agregando la entidad
+            _context.Entry(cancion).State = EntityState.Modified;
+
+            try
+            {
+                //Guardamos los cambios controlando hay una excepcion mirar si es porque no existe el id
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CancionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        private bool CancionExists(int id)
+        {
+            return _context.Canciones.Any(e => e.Id == id);
         }
 
     }
